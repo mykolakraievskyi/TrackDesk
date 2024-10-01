328407206f54c79ad97094def27a7c2738bf08fb
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-10-01 20:45:52 +0300

    setting up java

---
+++ TrackDesk.API/train-desk/src/main/java/com/if_core_034/train_desk/TrainDeskApplication.java
@@ -0,0 +1,13 @@
+package com.if_core_034.train_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainDeskApplication.class, args);
+	}
+
+}
---
+++ TrackDesk.API/train-desk/src/test/java/com/if_core_034/train_desk/TrainDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.if_core_034.train_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
