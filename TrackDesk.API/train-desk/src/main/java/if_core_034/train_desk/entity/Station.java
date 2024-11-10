2a651a5eeb75a66dc7b59571a7ec9a612cd3be6d
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-11 00:46:51 +0200

    adding generation strategy classes

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/entity/Station.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/entity/Station.java
@@ -3,8 +3,16 @@ package if_core_034.train_desk.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class Station {
-    
+    private Station instance;
+    private List<Entrance> entrances;
+    private List<CashDesk> cashDesks;
+    private CashDesk reserveCashDesk;
+    private TimeRange serviceTimeRange;
+    private int currClientNumber;
+    private int maxClientCapacity;
 }
---
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/strategy/RandomGenerationStrategy.java
@@ -0,0 +1,20 @@
+package if_core_034.train_desk.strategy;
+
+import if_core_034.train_desk.entity.TimeRange;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+public class IntervalGenerationStrategy implements ClientGenerationStrategy {
+    private TimeRange timeRange;
+
+    public LocalTime getNextArrivalTime() {
+        int minTimeSecondOfDay = timeRange.getMinTime().toSecondOfDay();
+        int maxTimeSecondOfDay = timeRange.getMaxTime().toSecondOfDay();
+        return LocalTime.ofSecondOfDay(minTimeSecondOfDay + (int) (Math.random() * (maxTimeSecondOfDay - minTimeSecondOfDay)));
+    }
+}
