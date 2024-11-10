d90ecedd0a517f59f9e654013c6946b55c22b272
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-10 23:38:01 +0200

    adding new client status logic

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/entity/ClientStatus.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/entity/ClientStatus.java
@@ -2,7 +2,7 @@ package if_core_034.train_desk.entity;
 
 public enum ClientStatus {
     REGULAR,
-    DISABLED,
-    WITH_CHILD,
     OTHER,
+    WITH_CHILD,
+    DISABLED,
 }
---
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/entity/Station.java
@@ -0,0 +1,10 @@
+package if_core_034.train_desk.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Station {
+    
+}
