4e148e1d844c4427b490c15d990537f299d2c366
VarenytsiaV <victoriavarenytsia@gmail.com>
2024-11-14 11:39:03 +0200

    TRAIN-20: Update models

--- TrackDesk.Web/train-desk/src/app/features/models/cash-desk.model.ts
+++ TrackDesk.Web/train-desk/src/app/features/models/cash-desk.model.ts
@@ -8,30 +8,26 @@ export interface CashDesk {
 }
 
 export class BaseCashDesk implements CashDesk {
-  id: number;
-  position: Position;
   image: string;
-  type: 'cash-desk' | 'closed-cash-desk' | 'ticket-box';
 
   constructor(
-    id: number,
-    position: Position,
-    type: 'cash-desk' | 'closed-cash-desk' | 'ticket-box'
+    public id: number,
+    public position: Position,
+    public type: 'cash-desk' | 'closed-cash-desk' | 'ticket-box'
   ) {
-    this.id = id;
-    this.position = position;
     this.image = this.getImagePath();
-    this.type = type;
   }
 
   private getImagePath(): string {
     switch (this.type) {
       case 'cash-desk':
-        return `assets/images/cash-desk/cash-desk.png`;
+        return 'assets/images/cash-desk/cash-desk.png';
       case 'closed-cash-desk':
-        return `assets/images/closed-cash-desk/closed-cash-desk.png`;
+        return 'assets/images/closed-cash-desk/closed-cash-desk.png';
       case 'ticket-box':
-        return `assets/images/ticket-box/ticket-box.png`;
+        return 'assets/images/ticket-box/ticket-box.png';
+      default:
+        return '';
     }
   }
 }
