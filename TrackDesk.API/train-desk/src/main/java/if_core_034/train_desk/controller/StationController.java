7b4b5f8bd2e3137bbee6279470ee2ea3d7717c4d
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-14 23:41:47 +0200

    adding sockets to station controller

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
@@ -1,11 +1,26 @@
 package if_core_034.train_desk.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class StationController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @MessageMapping("/message")
+    public void processMessage(@Payload String message) {
+        System.out.println(message);
+    }
+
     @GetMapping("/get/station")
     public ResponseEntity<String> getSomeInfo() {
         return ResponseEntity.ok("Some info");
