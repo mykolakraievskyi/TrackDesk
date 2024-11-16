a6d2183d17b67cf6043065637c915db340e46dad
Oleh Vorobets <olehcooperation@gmail.com>
2024-11-16 14:37:23 +0200

    Added basic train animation

--- TrackDesk.Web/train-desk/src/app/app.routes.ts
+++ TrackDesk.Web/train-desk/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { StationComponent } from './routes/station/station.component';
 import { HomeComponent } from './routes/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', redirectTo: 'home', component: HomeComponent },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'station', component: StationComponent, pathMatch: 'full' },
   { path: '**', redirectTo: 'home' },
 ];
