4edcafbc4310264c1a6397d2a2e6b4b0cbfeb7ae
Artem-Pliusnin <artemplyusnin2005@gmail.com>
2024-11-19 00:43:27 +0200

    Fixed log component design

--- TrackDesk.Web/train-desk/src/app/shared/components/log/log.component.ts
+++ TrackDesk.Web/train-desk/src/app/shared/components/log/log.component.ts
@@ -12,11 +12,11 @@ export class LogComponent {
    logsArray = [
     {
       name: 'Oleh',
-      status: 'Gey',
+      description: 'student',
     },
     {
       name: 'Nastya',
-      status: 'adult',
+      description: 'adult',
     }
   ];
 
