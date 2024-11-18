c0460e72a4c0c4ce0be6f7ea5f5b7cc141b0fec9
duck <->
2024-11-18 14:20:08 +0200

    fix cors

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
@@ -2,25 +2,26 @@ package if_core_034.train_desk.controller;
 
 import if_core_034.train_desk.dto.B2F;
 import if_core_034.train_desk.dto.F2B;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class StationController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired(required = false)
+    private F2B conf;
 
 
     @MessageMapping("/message")
@@ -33,8 +34,13 @@ public class StationController {
         simpMessagingTemplate.convertAndSendToUser(String.valueOf(1), "/open/message", "Open station");
     }
 
-    @PostMapping("/conf")
+        @PostMapping("/conf")
     public ResponseEntity<B2F> createCustomer(@RequestBody F2B conf) {
+//        if (this.conf != null) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+        this.conf = conf;
+        System.out.println(conf);
         Integer[] cashRegisters = {2, 3, 4};
         Integer[] entryPoints = {4, 5};
         Integer[] exitPoints = {6, 7, 8};
