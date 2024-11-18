56adb8e5bfe47b2c2f680ae855b99c169f7e26d7
duck <->
2024-11-18 13:45:49 +0200

    add F2B abd B2F dto, simple post endpoint

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
@@ -1,10 +1,16 @@
 package if_core_034.train_desk.controller;
 
+import if_core_034.train_desk.dto.B2F;
+import if_core_034.train_desk.dto.F2B;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -25,6 +31,18 @@ public class StationController {
     @Scheduled(fixedRate = 5000)
     public void sendOpenMsg() {
         simpMessagingTemplate.convertAndSendToUser(String.valueOf(1), "/open/message", "Open station");
+    }
+
+    @PostMapping("/conf")
+    public ResponseEntity<B2F> createCustomer(@RequestBody F2B conf) {
+        Integer[] cashRegisters = {2, 3, 4};
+        Integer[] entryPoints = {4, 5};
+        Integer[] exitPoints = {6, 7, 8};
+
+        B2F b2f = new B2F(cashRegisters, entryPoints, exitPoints);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(b2f);
     }
 
     @GetMapping("/get/station")
