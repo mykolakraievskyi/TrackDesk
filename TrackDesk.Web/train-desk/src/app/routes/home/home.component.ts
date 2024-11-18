daa2a5617219b5f1a264e052b76fb6c5028eb888
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-11-18 19:28:28 +0200

    pretty?

--- TrackDesk.Web/train-desk/src/app/routes/home/home.component.ts
+++ TrackDesk.Web/train-desk/src/app/routes/home/home.component.ts
@@ -11,55 +11,73 @@ import { ConfigurationService } from '../../shared/services/configuration.servic
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  constructor(private router: Router, private configurationService: ConfigurationService) {}
+  constructor(
+    private router: Router,
+    private configurationService: ConfigurationService
+  ) {}
 
-  Exit = "";
-  Entry = "";
-  CashRegisters = "";
-  secondsStart = "";
-  secondsEnd = "";
-  timeOption = "random";
-
+  Exit = '';
+  Entry = '';
+  CashRegisters = '';
+  secondsStart = '';
+  secondsEnd = '';
+  timeOption = 'random';
 
   onStartClick() {
-    if(this.validateData() === true){
-    if(this.timeOption === "random"){
-      this.configurationService.setConfiguration(+this.CashRegisters, +this.Entry, +this.Exit, +this.secondsStart, +this.secondsEnd).subscribe({
-        next: (response) => console.log('Запит успішний:', response),
-        error: (error) => console.error('Помилка:', error),
-      });
-    } else if(this.timeOption === "static"){
-        this.configurationService.setConfiguration(+this.CashRegisters, +this.Entry, +this.Exit, +this.secondsStart, +this.secondsStart).subscribe({
-          next: (response) => console.log('Запит успішний:', response),
-          error: (error) => console.error('Помилка:', error),
-        });
+    if (this.validateData() === true) {
+      if (this.timeOption === 'random') {
+        this.configurationService
+          .setConfiguration(
+            +this.CashRegisters,
+            +this.Entry,
+            +this.Exit,
+            +this.secondsStart,
+            +this.secondsEnd
+          )
+          .subscribe({
+            next: response => console.log('Запит успішний:', response),
+            error: error => console.error('Помилка:', error),
+          });
+      } else if (this.timeOption === 'static') {
+        this.configurationService
+          .setConfiguration(
+            +this.CashRegisters,
+            +this.Entry,
+            +this.Exit,
+            +this.secondsStart,
+            +this.secondsStart
+          )
+          .subscribe({
+            next: response => console.log('Запит успішний:', response),
+            error: error => console.error('Помилка:', error),
+          });
       }
-    this.router.navigate(['station']);
+      this.router.navigate(['station']);
     }
   }
 
-  validateData(): boolean{
-    if(+this.Exit < 2 || +this.Exit>9){
+  validateData(): boolean {
+    if (+this.Exit < 2 || +this.Exit > 9) {
       return false;
     }
-    if(+this.Entry < 2 || +this.Entry>9){
+    if (+this.Entry < 2 || +this.Entry > 9) {
       return false;
     }
-    if(+this.CashRegisters < 1 || +this.CashRegisters > 11){
+    if (+this.CashRegisters < 1 || +this.CashRegisters > 11) {
       return false;
     }
-    if(this.timeOption === "static"){
-      if(+this.secondsStart < 2 || +this.secondsStart > 10){
-        return false
+    if (this.timeOption === 'static') {
+      if (+this.secondsStart < 2 || +this.secondsStart > 10) {
+        return false;
       }
-    }else if(this.timeOption === "random"){
-      if(+this.secondsStart < 2 || +this.secondsStart > 10){
-        return false
+    } else if (this.timeOption === 'random') {
+      if (+this.secondsStart < 2 || +this.secondsStart > 10) {
+        return false;
       }
-      if(+this.secondsEnd < 2 || +this.secondsEnd > 10){
-        return false
+      if (+this.secondsEnd < 2 || +this.secondsEnd > 10) {
+        return false;
       }
-      if(+this.secondsStart > +this.secondsEnd){
+      if (+this.secondsStart > +this.secondsEnd) {
         return false;
       }
     }
