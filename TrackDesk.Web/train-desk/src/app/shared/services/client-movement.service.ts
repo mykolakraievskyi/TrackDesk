af11fdb188807fe7e967eb37e9d76b9529260c02
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-11-18 22:04:52 +0200

    added queue

--- TrackDesk.Web/train-desk/src/app/shared/services/client-movement.service.ts
+++ TrackDesk.Web/train-desk/src/app/shared/services/client-movement.service.ts
@@ -1,18 +1,38 @@
+import { transition } from '@angular/animations';
 import { CashDesk } from '../../features/models/cash-desk.model';
 import { Client } from '../../features/models/client.model';
+import { CLIENT_WIDTH_PX } from '../../features/models/obstacle.model';
 
+const QUEUE_OFFSET = 32;
 export class MovementService {
-  private readonly speed: number = 5; 
+  private readonly speed: number = 5;
 
-  moveClientToCashDesk(client: Client, cashDesk: CashDesk): void {
-    client.targetCashDeskId = cashDesk.id; 
+  detectCollisions(): void {}
+
+  moveClientToCashDesk(
+    client: Client,
+    cashDesk: CashDesk,
+    allClients: Client[],
+    allCashDesks: CashDesk[]
+  ): void {
+    client.targetCashDeskId = cashDesk.id;
     const moveInterval = setInterval(() => {
-      const deltaX = cashDesk.position.x - client.position.x;
-      const deltaY = cashDesk.position.y - client.position.y;
+      const lastClientPosition = cashDesk.getClientPosition(client);
+      const targetPosition = {
+        x: lastClientPosition.x,
+        y: lastClientPosition.y + QUEUE_OFFSET,
+      };
+
+      // console.log(targetPosition);
+      // console.log(cashDesk.clientQueue);
+
+      const deltaX = targetPosition.x - client.position.x;
+      const deltaY = targetPosition.y - client.position.y;
 
       if (Math.abs(deltaX) <= this.speed && Math.abs(deltaY) <= this.speed) {
-        client.position.x = cashDesk.position.x;
-        client.position.y = cashDesk.position.y;
+        client.position.x = targetPosition.x;
+        client.position.y = targetPosition.y;
+        this.serveClient(client, cashDesk);
         clearInterval(moveInterval);
         return;
       }
@@ -21,7 +41,7 @@ export class MovementService {
         Math.sign(deltaX) * Math.min(this.speed, Math.abs(deltaX));
       client.position.y +=
         Math.sign(deltaY) * Math.min(this.speed, Math.abs(deltaY));
-    }, 100); 
+    }, 100);
   }
 
   findCashDeskWithFewestClients(
@@ -45,9 +65,38 @@ export class MovementService {
   assignClientToBestCashDesk(
     client: Client,
     cashDesks: CashDesk[],
-    clients: Client[]
+    clients: Client[],
+    allClients: Client[],
+    allCashDesks: CashDesk[]
   ): void {
     const bestCashDesk = this.findCashDeskWithFewestClients(cashDesks, clients);
-    this.moveClientToCashDesk(client, bestCashDesk);
+    this.moveClientToCashDesk(client, bestCashDesk, allClients, allCashDesks);
+  }
+
+  // )))))))
+  serveClient(client: Client, cashDesk: CashDesk): void {
+    console.log('serving');
+    if (!cashDesk.clientQueue.includes(client)) {
+      console.log('inserting');
+
+      cashDesk.addClient(client);
+    }
+
+    // Тут виклик сервісу для обслуговування
+
+    // var deadClient = cashDesk.popClient();
+
+    // if (deadClient) {
+    //   for (let i = cashDesk.clientQueue.length - 1; i >= 0; ++i) {
+    //     if (i > 0) {
+    //       cashDesk.clientQueue[i - 1].position =
+    //         cashDesk.clientQueue[i].position;
+    //     } else {
+    //       cashDesk.clientQueue[i].position = deadClient.position;
+    //     }
+    //   }
+    // } else {
+    //   // похуй
+    // }
   }
 }
