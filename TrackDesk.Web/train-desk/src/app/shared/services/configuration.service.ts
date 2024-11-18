17721dc1edff527b128f78a95bec7502554f5dcd
mykolakraievskyi <m.kraievskyi@gmail.com>
2024-11-19 00:29:40 +0200

    conf service fe

--- TrackDesk.Web/train-desk/src/app/shared/services/configuration.service.ts
+++ TrackDesk.Web/train-desk/src/app/shared/services/configuration.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Position } from '../../features/models/position.model';
 
+export interface ConfigurationRequest {
+  cashDesks: EntityDto[];
+  entrances: EntityDto[];
+  secondsStart: number;
+  secondsEnd: number;
+}
+interface EntityDto {
+  id: number;
+  position: Position;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigurationService {
-  private apiUrl = 'https://'; 
+  private apiUrl = 'https://';
 
   constructor(private http: HttpClient) {}
 
-  configuration:Observable<any>|null = null;
+  configuration: Observable<any> | null = null;
 
   setConfiguration(
     CashRegisters: number,
@@ -19,22 +30,19 @@ export class ConfigurationService {
     secondsStart: number,
     secondsEnd: number
   ): Observable<any> {
-    return this.configuration = this.http.post(`http://127.0.0.1:8080/conf`, {
+    return (this.configuration = this.http.post(`http://127.0.0.1:8080/conf`, {
       CashRegisters,
       Entry,
       Exit,
       secondsStart,
       secondsEnd,
-    });
+    }));
   }
-  
+
   getConfiguration(): Observable<any> | null {
     if (!this.configuration) {
       console.warn('Configuration has not been set.');
     }
     return this.configuration;
   }
-
-
-
 }
