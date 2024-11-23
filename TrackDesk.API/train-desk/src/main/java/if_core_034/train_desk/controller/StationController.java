b70fea610186f6caff0b3ac58fe308b532169c36
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-23 20:28:24 +0200

    adding response to setting configuration correctly

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/StationController.java
@@ -3,6 +3,7 @@ package if_core_034.train_desk.controller;
 import if_core_034.train_desk.dto.StationConfigurationDto;
 import if_core_034.train_desk.service.StationService;
 import if_core_034.train_desk.dto.StationOpenCloseDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -31,8 +32,9 @@ public class StationController {
 //    }
 
     @PostMapping("/api/v1/configuration")
-    public void setStationConfiguration(@RequestBody StationConfigurationDto stationConfigurationDto) {
+    public ResponseEntity<Object> setStationConfiguration(@RequestBody StationConfigurationDto stationConfigurationDto) {
         stationService.createStationInstance(stationConfigurationDto);
+        return ResponseEntity.ok().build();
     }
 
 //    @MessageMapping("/message")
