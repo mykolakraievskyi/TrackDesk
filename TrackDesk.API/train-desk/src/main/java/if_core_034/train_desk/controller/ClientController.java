793a311c920c43ae26fb2fab9a6a70ebd3814f0e
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-23 23:13:36 +0200

    adding cashdesk queue filling by new clients logic

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/ClientController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/ClientController.java
@@ -42,6 +42,7 @@ public class ClientController {
     public void generateClient() {
         Client client = clientService.generateClient();
         int cashDeskId = clientService.getBestCashRegisterId(client);
+        stationService.getStationInstance().getCashDesks().get(cashDeskId).getQueue().add(client);
         ClientDto clientDto = new ClientDto(client.getId(), client.getStatus(), cashDeskId, client.getEntrance().getId());
         simpMessagingTemplate.convertAndSendToUser("standardUser", "/client/generate", clientDto);
         Station station = stationService.getStationInstance();
