6509f76ffb155e652158a191fdc5942ca746dd53
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-23 16:23:01 +0200

    adding controller socket random time generation logic

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/ClientController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/ClientController.java
@@ -1,27 +1,71 @@
 package if_core_034.train_desk.controller;
 
+import if_core_034.train_desk.dto.CashDeskDto;
 import if_core_034.train_desk.dto.ClientDto;
+import if_core_034.train_desk.dto.StationConfigurationDto;
 import if_core_034.train_desk.entity.Client;
+import if_core_034.train_desk.entity.Entrance;
+import if_core_034.train_desk.entity.Position;
+import if_core_034.train_desk.entity.Station;
 import if_core_034.train_desk.service.ClientService;
+import if_core_034.train_desk.service.StationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 
-import lombok.RequiredArgsConstructor;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final StationService stationService;
 
-    @Scheduled(fixedRate = 5000, initialDelay = 5000)
+    @Autowired
+    public ClientController(ClientService clientService, SimpMessagingTemplate simpMessagingTemplate,
+                            StationService stationService) {
+        this.clientService = clientService;
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.stationService = stationService;
+        ClientGeneration clientGeneration = new ClientGeneration();
+        clientGeneration.run();
+    }
+
+
+//    @Scheduled(fixedRateString = "#{clientService.nextArrivalTime}", initialDelay = 5000)
     public void generateClient() {
-        Client client = clientService.generateClient();
-        ClientDto clientDto = new ClientDto(client.getId(), client.getStatus(), client.getEntrance().getId());
-        simpMessagingTemplate.convertAndSendToUser("standardUser", "/client/generate", clientDto);
+        if(stationService.isInitialized().get()) {
+            Client client = clientService.generateClient();
+            ClientDto clientDto = new ClientDto(client.getId(), client.getStatus(), 1, client.getEntrance().getId());
+            simpMessagingTemplate.convertAndSendToUser("standardUser", "/client/generate", clientDto);
+            Station station = stationService.getStationInstance();
+            station.setCurrClientNumber(station.getCurrClientNumber() + 1);
+
+        } else {
+            List<CashDeskDto> cashDeskDtos = new ArrayList<>();
+            List<Entrance> entrances = new ArrayList<>();
+            for(int i = 1; i <= 3; i++) {
+                cashDeskDtos.add(new CashDeskDto(i, new Position(i * 10, i * 10)));
+                entrances.add(new Entrance(i, new Position(i * 10, i * 10)));
+            }
+            StationConfigurationDto stationConfigurationDto = new StationConfigurationDto(cashDeskDtos, entrances, 15, 15);
+            stationService.createStationInstance(stationConfigurationDto);
+        }
+    }
+
+    private class ClientGeneration extends TimerTask {
+        @Override
+        public void run() {
+            long delay = clientService.getNextArrivalTime();
+            Timer timer = new Timer();
+            generateClient();
+            timer.schedule(new ClientGeneration(), delay);
+        }
     }
 }
