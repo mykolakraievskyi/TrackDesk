991963cb924c510833ec64f3ca417f80e7a53094
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-23 16:27:50 +0200

    adding strategy component initialization logic

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/strategy/RandomGenerationStrategy.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/strategy/RandomGenerationStrategy.java
@@ -2,13 +2,16 @@ package if_core_034.train_desk.strategy;
 
 import if_core_034.train_desk.entity.TimeRange;
 
-import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalTime;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
+@Component
 public class RandomGenerationStrategy implements ClientGenerationStrategy {
     private TimeRange timeRange;
 
@@ -16,5 +19,9 @@ public class RandomGenerationStrategy implements ClientGenerationStrategy {
         int minTimeSecondOfDay = timeRange.getMinTime().toSecondOfDay();
         int maxTimeSecondOfDay = timeRange.getMaxTime().toSecondOfDay();
         return LocalTime.ofSecondOfDay(minTimeSecondOfDay + (int) (Math.random() * (maxTimeSecondOfDay - minTimeSecondOfDay)));
+    }
+
+    public void updateTimeRange(TimeRange timeRange) {
+        this.timeRange = timeRange;
     }
 }
