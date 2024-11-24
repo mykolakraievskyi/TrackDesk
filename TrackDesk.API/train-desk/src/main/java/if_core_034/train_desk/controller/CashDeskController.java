8f5ba6cc630dde615217388f752ceb4a34af798f
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-24 22:15:21 +0200

    adding logging client information logic

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/CashDeskController.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/controller/CashDeskController.java
@@ -1,7 +1,12 @@
 package if_core_034.train_desk.controller;
 
 import if_core_034.train_desk.dto.CashDeskOpenCloseDto;
+import if_core_034.train_desk.entity.CashDesk;
+import if_core_034.train_desk.entity.Client;
+import if_core_034.train_desk.entity.LogEntity;
 import if_core_034.train_desk.service.CashDeskService;
+import if_core_034.train_desk.entity.Station;
+import if_core_034.train_desk.service.LogEntityService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import if_core_034.train_desk.dto.BuyTicketDto;
@@ -13,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 public class CashDeskController {
-    private final SimpMessagingTemplate simpMessagingTemplate;
     private final CashDeskService cashDeskService;
     private final StationService stationService;
+    private final LogEntityService logEntityService;
 
 //    @Scheduled(fixedRate = 30000, initialDelay = 15000)
 //    public void closeRandomCashDesk() {
@@ -58,11 +65,25 @@ public class CashDeskController {
     //            }, 15, TimeUnit.SECONDS);
         }*/
     @PostMapping("/api/v1/cashdesk/buy/ticket")
-    public ResponseEntity<Object> buyTicket(@RequestBody BuyTicketDto buyTicketDTO) {
+    public ResponseEntity<Object> buyTicket(@RequestBody BuyTicketDto buyTicketDto) {
         //TODO якщо 0 то забрати з резервної каси
-        stationService.getStationInstance().getCashDeskMap().get(buyTicketDTO.getCashDeskId())
-                .getQueue().removeIf(client -> client.getId() == buyTicketDTO.getClientId());
-        System.out.println(buyTicketDTO);
-        return ResponseEntity.ok().build();
+        Station station = stationService.getStationInstance();
+        CashDesk cashDesk;
+        if(buyTicketDto.getCashDeskId() != 0) {
+            cashDesk = station.getCashDeskMap().get(buyTicketDto.getCashDeskId());
+            System.out.println(buyTicketDto);
+        } else {
+            cashDesk = station.getReserveCashDesk();
+        }
+        Optional<Client> client = cashDesk.getQueue().stream().filter(clientNew ->
+                                                               clientNew.getId() == buyTicketDto.getClientId()).findFirst();
+
+        if(client.isPresent()) {
+            LogEntity logEntity = new LogEntity(0, client.get().getId(), client.get().getStatus(), cashDesk.getId(),
+                                                   client.get().getTickets(), buyTicketDto.getStartTime(), buyTicketDto.getEndTime());
+            logEntityService.saveLogEntity(logEntity);
+            return ResponseEntity.ok().body(logEntity);
+        }
+        return ResponseEntity.badRequest().body("Client with id - " + buyTicketDto.getClientId() + " does not found");
     }
 }
