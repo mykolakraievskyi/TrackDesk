43dec01b0ff367af154a4490567c8b9da8dcb77b
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2024-11-24 18:48:27 +0200

    fixed tests

--- TrackDesk.API/train-desk/src/test/java/if_core_034/train_desk/StationTest.java
+++ TrackDesk.API/train-desk/src/test/java/if_core_034/train_desk/StationTest.java
@@ -10,7 +10,9 @@ import static org.mockito.Mockito.mock;
 
 import java.time.LocalTime;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class StationTest {
     Entrance entranceMock = mock(Entrance.class);
@@ -18,10 +20,12 @@ class StationTest {
     TimeRange timeRangeMock = mock(TimeRange.class);
 
     @Test
-    void testInitializationParameters() {;
+    void testInitializationParameters() {
+        Map<Integer, CashDesk> cashDeskMap = Map.of(1, cashDeskMock, 2, cashDeskMock);
+
         Station station = Station.getInstance(
                 List.of(entranceMock),
-                List.of(cashDeskMock),
+                cashDeskMap,
                 cashDeskMock,
                 timeRangeMock,
                 5,
@@ -29,7 +33,9 @@ class StationTest {
         );
 
         assertEquals(1, station.getEntrances().size(), "Entrances size should be 1");
-        assertEquals(1, station.getCashDesks().size(), "CashDesks size should be 1");
+        assertEquals(2, station.getCashDeskMap().size(), "CashDeskMap size should be 2");
+        assertSame(cashDeskMock, station.getCashDeskMap().get(1), "CashDesk with key 1 should match");
+        assertSame(cashDeskMock, station.getCashDeskMap().get(2), "CashDesk with key 2 should match");
         assertNotNull(station.getReserveCashDesk(), "ReserveCashDesk should not be null");
         assertEquals(timeRangeMock, station.getServiceTimeRange(), "ServiceTimeRange should match");
         assertEquals(5, station.getCurrClientNumber(), "Current client number should match");
@@ -37,11 +43,12 @@ class StationTest {
     }
 
 
+
     @Test
     void testSingletonProperty() {
         Station station1 = Station.getInstance(
                 Collections.emptyList(),
-                Collections.emptyList(),
+                new HashMap<>(),
                 null,
                 new TimeRange(LocalTime.of(10, 30), LocalTime.of(11, 30)),
                 0,
@@ -49,7 +56,7 @@ class StationTest {
         );
         Station station2 = Station.getInstance(
                 Collections.emptyList(),
-                Collections.emptyList(),
+                new HashMap<>(),
                 null,
                 new TimeRange(LocalTime.of(10, 30), LocalTime.of(11, 30)),
                 0,
