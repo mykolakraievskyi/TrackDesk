2d8a22b14c553e5d1f50cb621309f878122037ed
Oleh Vorobets <olehcooperation@gmail.com>
2024-11-24 22:23:37 +0200

    Fixed bug

--- TrackDesk.Web/train-desk/src/app/routes/station/station.component.ts
+++ TrackDesk.Web/train-desk/src/app/routes/station/station.component.ts
@@ -40,6 +40,7 @@ export class StationComponent implements OnInit, OnDestroy {
   private clientService = inject(ClientService);
   private initService = inject(InitService);
   private socketService = inject(StompService);
+  private anyDeskClosed: any = null;
 
   constructor(private router: Router) {}
 
@@ -155,11 +156,20 @@ export class StationComponent implements OnInit, OnDestroy {
   }
 
   toggleDeskClosing(cashDesk: CashDesk) {
-    cashDesk.isClosed = !cashDesk.isClosed;
-    this.socketService.emit('/cashdesk/action', {
-      isClosed: cashDesk.isClosed,
-      id: cashDesk.id,
-    });
-    return cashDesk.isClosed;
+    if (this.anyDeskClosed === cashDesk) {
+      this.anyDeskClosed = null;
+      cashDesk.isClosed = false;
+    } else {
+      if (!this.anyDeskClosed) {
+        this.anyDeskClosed = cashDesk;
+        cashDesk.isClosed = !cashDesk.isClosed;
+        this.socketService.emit('/cashdesk/action', {
+          isClosed: cashDesk.isClosed,
+          id: cashDesk.id,
+        });
+        return cashDesk.isClosed;
+      }
+    }
+    return false;
   }
 }
