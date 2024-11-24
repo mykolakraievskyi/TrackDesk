90fb85cb68fcaa8c8dbcab340e4f3ec4e22e7cbc
OlehKulyniak <oleh.kulyniak.pz.2022@lpnu.ua>
2024-11-24 22:17:45 +0200

    adding start and end serving time fields in buyticketdto

--- TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/dto/BuyTicketDto.java
+++ TrackDesk.API/train-desk/src/main/java/if_core_034/train_desk/dto/BuyTicketDto.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BuyTicketDto {
     private int clientId;
     private int cashDeskId;
+    private LocalTime startTime;
+    private LocalTime endTime;
 }
